1d68c771a3a7a478eb38396ce67537bdf410905f
Anas Alam <anasalam027@gmail.com>
2025-04-09 03:04:51 +0530

    changes

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -107,112 +107,106 @@ export default function Login() {
   };
 
   return (
-    <>
-      <Marquee className="py-4 bg-[#FFED29]/80 text-gray-700 font-medium">
-        If you are unable to login Please try to reset your password.
-        Inconvenience is regretted.
-      </Marquee>
-      <div className="flex min-h-screen relative">
-        {/* Left Section */}
-        <div className="flex flex-col items-center  w-full lg:w-1/2 p-8">
-          {/* Logo */}
-          <div className=" self-start p-10 relative z-10 mt-6">
-            <Link href="/">
-              <Image
-                src={assets.logo} // Replace with your logo path
-                alt="Kushi Cricket Store Logo"
-                width={150}
-                height={50}
-              />
-            </Link>
-          </div>
-          <div className="w-[98%] absolute h-[200px] bg-gradient-to-r from-[#2C8EE31C] to-[#2C8EE300] rounded-lg left-[50%] translate-x-[-50%] top-5"></div>
-          {/* Login Form */}
-          <div className="bg-white  rounded-lg w-full max-w-md p-6 m-auto">
-            <h2 className="text-2xl font-semibold text-center text-gray-800 mb-2">
-              Sign in to the platform
-            </h2>
-            <p className="text-center text-gray-600 mb-6">
-              Enter your credentials to continue
-            </p>
+    <div className="flex min-h-screen relative">
+      {/* Left Section */}
+      <div className="flex flex-col items-center  w-full lg:w-1/2 p-8">
+        {/* Logo */}
+        <div className=" self-start p-10 relative z-10 mt-6">
+          <Link href="/">
+            <Image
+              src={assets.logo} // Replace with your logo path
+              alt="Kushi Cricket Store Logo"
+              width={150}
+              height={50}
+            />
+          </Link>
+        </div>
+        <div className="w-[98%] absolute h-[200px] bg-gradient-to-r from-[#2C8EE31C] to-[#2C8EE300] rounded-lg left-[50%] translate-x-[-50%] top-5"></div>
+        {/* Login Form */}
+        <div className="bg-white  rounded-lg w-full max-w-md p-6 m-auto">
+          <h2 className="text-2xl font-semibold text-center text-gray-800 mb-2">
+            Sign in to the platform
+          </h2>
+          <p className="text-center text-gray-600 mb-6">
+            Enter your credentials to continue
+          </p>
 
-            {/* Email Input */}
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <CustomInput
-                id="email"
-                text="Email Address"
-                type="email"
-                placeholder="johndoe@example.com"
-                validationProps={register("email")}
-                error={errors?.email?.message || ""}
-                isRequired={true}
-              />
-              <CustomInput
-                id="password"
-                text="Password"
-                type="password"
-                placeholder="Enter Password"
-                validationProps={register("password")}
-                error={errors?.password?.message || ""}
-                isRequired={true}
-              />
-              {/* Remember Me and Forgot Password */}
-              <div className="flex items-center justify-end mb-4">
-                {/* <label className="flex items-center">
+          {/* Email Input */}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <CustomInput
+              id="email"
+              text="Email Address"
+              type="email"
+              placeholder="johndoe@example.com"
+              validationProps={register("email")}
+              error={errors?.email?.message || ""}
+              isRequired={true}
+            />
+            <CustomInput
+              id="password"
+              text="Password"
+              type="password"
+              placeholder="Enter Password"
+              validationProps={register("password")}
+              error={errors?.password?.message || ""}
+              isRequired={true}
+            />
+            {/* Remember Me and Forgot Password */}
+            <div className="flex items-center justify-end mb-4">
+              {/* <label className="flex items-center">
                 <input
                   type="checkbox"
                   className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                 />
                 <span className="ml-2 text-sm text-gray-700">Remember me</span>
               </label> */}
-                <Link
-                  href="/auth/forgot-password"
-                  className="text-sm  hover:underline"
-                >
-                  Forgot password?
-                </Link>
-              </div>
-
-              {/* Submit Button */}
-              <Button
-                type="submit"
-                className="w-full px-4 py-2 !text-white !bg-primary !shadow-[0px_5px_50px_0px_#2C8EE380]"
-                isLoading={isPending}
-              >
-                Log In
-              </Button>
-            </form>
-
-            {/* Sign Up Link */}
-            <p className="mt-6 text-sm text-center text-gray-600">
-              Don&apos;t have an account?{" "}
               <Link
-                href="/auth/signup"
-                className="text-primary font-semibold hover:underline"
+                href="/auth/forgot-password"
+                className="text-sm  hover:underline"
               >
-                Sign up here
+                Forgot password?
               </Link>
-            </p>
-          </div>
-        </div>
+            </div>
 
-        {/* Right Section */}
-        <div className="hidden lg:flex lg:w-1/2 bg-blue-200 items-center justify-center">
-          <div className=" relative z-10 rounded-lg pr-8">
-            <Image
-              src={assets.loginImage} // Replace with your image path
-              alt="Tennis court"
-              width={748}
-              height={936}
-              objectFit="cover"
-              className="rounded-lg h-[700px]"
-              style={{
-                border: "8px solid #fff"
-              }}
-            />
-          </div>
+            {/* Submit Button */}
+            <Button
+              type="submit"
+              className="w-full px-4 py-2 !text-white !bg-primary !shadow-[0px_5px_50px_0px_#2C8EE380]"
+              isLoading={isPending}
+            >
+              Log In
+            </Button>
+          </form>
+
+          {/* Sign Up Link */}
+          <p className="mt-6 text-sm text-center text-gray-600">
+            Don&apos;t have an account?{" "}
+            <Link
+              href="/auth/signup"
+              className="text-primary font-semibold hover:underline"
+            >
+              Sign up here
+            </Link>
+          </p>
         </div>
       </div>
-    </>
+
+      {/* Right Section */}
+      <div className="hidden lg:flex lg:w-1/2 bg-blue-200 items-center justify-center">
+        <div className=" relative z-10 rounded-lg pr-8">
+          <Image
+            src={assets.loginImage} // Replace with your image path
+            alt="Tennis court"
+            width={748}
+            height={936}
+            objectFit="cover"
+            className="rounded-lg h-[700px]"
+            style={{
+              border: "8px solid #fff"
+            }}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
