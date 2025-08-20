b527c3dbd27a4913476a12bd21edcf09e41f5b8b
Anas Alam <anasalam027@gmail.com>
2025-08-20 19:12:49 +0530

    site-down

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import About from "@/components/Homepage/About";
-import Benefits from "@/components/Homepage/Benefits";
-import HeroSection from "@/components/Homepage/HeroSection";
-import Sponsored from "@/components/Homepage/Sponsored";
-// import PackageDetails from "@/components/Homepage/Package";
-// import SeasonPass from "@/components/Homepage/SeasonPass";
-import WhyUs from "@/components/Homepage/WhyUs";
-import AppLayout from "@/layouts/AppLayout";
+// import About from "@/components/Homepage/About";
+// import Benefits from "@/components/Homepage/Benefits";
+// import HeroSection from "@/components/Homepage/HeroSection";
+// import Sponsored from "@/components/Homepage/Sponsored";
+// import WhyUs from "@/components/Homepage/WhyUs";
+// import AppLayout from "@/layouts/AppLayout";
 
 export default function Home() {
   return <></>;
